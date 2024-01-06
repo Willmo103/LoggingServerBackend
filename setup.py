f52ee3ac824c04248d383b038a88d6e5c3a15d10
Will Morris <willmorris103@gmail.com>
2024-01-05 22:16:30 -0600

    Ready to GO again

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        install.run(self)
+        os.system("logging-api init")
+
 
 setup(
     name="logging_api",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["flask", "flask_sqlalchemy"],
+    install_requires=["flask", "flask_sqlalchemy", "click"],
+    cmdclass={
+        "install": PostInstallCommand,
+    },
+    entry_points="""
+    [console_scripts]
+    logging-api=your_package.cli:cli
+    logging-api-init=your_package.cli:init
+""",
 )
